f9ff14c1cbfb3c49e3f5b67162f8fbde0734c831
ozgunsandal <ozgunsandal@gmail.com>
2019-06-13 23:19:32 +0300

    Create autoInstaLike.js

---
+++ autoInstaLike.js
@@ -0,0 +1,5 @@
+document.querySelector(".eLAPa").click();
+setInterval(function(){
+document.querySelector('a.coreSpriteRightPaginationArrow').click();
+setTimeout(function(){document.querySelector('body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button > span').click();},1500);
+},3000);
